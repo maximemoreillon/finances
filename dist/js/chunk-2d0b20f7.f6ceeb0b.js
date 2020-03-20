c3925156aadacc569c5f17c83ef988d5d1d508ba
Maxime Moreillon <m.moreillon@gmail.com>
2020-03-20 06:27:41 +0000

    new template

--- dist/js/chunk-2d0b20f7.f6ceeb0b.js
+++ dist/js/chunk-2d0b20f7.f6ceeb0b.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b20f7"],{2300:function(n,t,a){"use strict";a.r(t);var c=function(){var n=this,t=n.$createElement,a=n._self._c||t;return a("Transactions",{attrs:{account:n.account_name}})},e=[],o=a("c102"),s={name:"CreditCardTransactions",components:{Transactions:o["a"]},computed:{account_name:function(){return"ts3"}}},r=s,u=a("2877"),i=Object(u["a"])(r,c,e,!1,null,"1aff5d2e",null);t["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d0b20f7.2a89bb8e.js.map
+//# sourceMappingURL=chunk-2d0b20f7.f6ceeb0b.js.map
