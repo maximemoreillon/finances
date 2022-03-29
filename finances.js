8b4f4c3f45b66bcdd70a106351d4ec1ebc9c6b87
Maxime Moreillon <m.moreillon@gmail.com>
2022-03-29 15:13:46 +0000

    new auth middleware

--- finances.js
+++ finances.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
-const auth = require('@moreillon/authentication_middleware')
+// const auth = require('@moreillon/authentication_middleware')
+const auth = require('@moreillon/express_identification_middleware')
 const group_auth = require('@moreillon/express_group_based_authorization_middleware')
 const {connect: mongodb_connect, url: mongodb_url, db: mongodb_db} = require('./mongodb.js')
 const {url: influxdb_url, db: influxdb_db} = require('./influxdb.js')
@@ -22,7 +23,7 @@ const {
 process.env.TZ = 'Asia/Tokyo'
 
 // configure the authorization middleware
-auth.authentication_api_url = `${AUTHENTICATION_API_URL}/decode_jwt`
+// auth.authentication_api_url = `${AUTHENTICATION_API_URL}/decode_jwt`
 
 mongodb_connect()
 
@@ -50,15 +51,15 @@ app.get('/', (req,res) => {
 })
 
 // Authenticate everything from here
-if(process.env.NODE_ENV !== 'development') app.use(auth.middleware)
-// if(AUTHORIZED_GROUPS && GROUP_AUTHORIZATION_URL) {
-//   console.log(`[Auth] Enabling group-based authorization`)
-//   const group_auth_options = {
-//     url: GROUP_AUTHORIZATION_URL,
-//     groups: AUTHORIZED_GROUPS.split(',')
-//   }
-//   app.use(group_auth(group_auth_options))
-// }
+if(process.env.NODE_ENV !== 'development') app.use(auth({url: AUTHENTICATION_URL}))
+if(AUTHORIZED_GROUPS && GROUP_AUTHORIZATION_URL) {
+  console.log(`[Auth] Enabling group-based authorization`)
+  const group_auth_options = {
+    url: GROUP_AUTHORIZATION_URL,
+    groups: AUTHORIZED_GROUPS.split(',')
+  }
+  app.use(group_auth(group_auth_options))
+}
 
 
 app.use('/accounts', require('./routes/accounts.js'))
