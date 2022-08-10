9de39b7b6fb84d9c32b8df10b7c1e5b30252d792
Maxime Moreillon <m.moreillon@gmail.com>
2022-08-10 13:48:26 +0000

    read balance

--- controllers/balance.js
+++ controllers/balance.js
@@ -57,9 +57,66 @@ exports.register_balance = async (req,res, next) => {
   }
 }
 
-exports.get_balance_history = (req,res) => {
+exports.get_balance_history = async (req, res, next) => {
 
-  res.status(501).send('not implemented')
+  try {
+
+    // TODO: use params
+    const account = req.query.account
+      || req.query.account_name
+      || req.params.account
+      || req.params.account_name
+
+    // Filters
+    // Using let because some variable types might change
+    let {
+      start = '0', // by default, query all points
+      stop,
+      tags = [],
+      fields = [],
+      limit = 500, // Limit point count by default, note: this is approximative
+    } = req.query
+
+    const stop_query = stop ? (`stop: ${stop}`) : ''
+
+
+    // If only one tag provided, will be parsed as string so put it in an array
+    if (typeof tags === 'string') tags = [tags]
+    if (typeof fields === 'string') fields = [fields]
+
+    // NOTE: check for risks of injection
+    let query = `
+      from(bucket:"${bucket}")
+      |> range(start: ${start}, ${stop_query})
+      |> filter(fn: (r) => r._measurement == "${account}")
+      `
+
+
+    // subsampling
+    if (Number(limit)) {
+      // Getting point count to compute the sampling from the limit
+      const count_query = query + `|> count()`
+      const record_count_query_result = await influx_read(count_query)
+      const record_count = record_count_query_result[0]._value // Dirty here
+      const sampling = Math.max(Math.round(12 * record_count / (limit)), 1) // Not sure why 12
+
+      // Apply subsampling
+      query += `|> sample(n:${sampling})`
+    }
+
+
+    // Run the query
+    const points = await influx_read(query)
+
+    // Respond to client
+    res.send(points)
+
+    console.log(`Balance history of account ${account} queried`)
+  }
+  catch (error) {
+    next(error)
+  }
+
 
 }
 
